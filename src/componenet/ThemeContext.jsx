91f53acceb2dd295b15f8138a9ce892ba6e21ce6
devturakhiya2006 <dev.turakhiya2006@gmail.com>
2026-06-01 00:08:57 +0530

    remove h2

--- src/componenet/ThemeContext.jsx
+++ src/componenet/ThemeContext.jsx
@@ -9,7 +9,6 @@ const ThemeContext = (props) => {
   return (
     <div>
       <DataPost.Provider value={[theme,settheme]}>
-        <h2>that is theme </h2>
         {props.children}
       </DataPost.Provider>
     </div>
